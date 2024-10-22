b43b76db7ac5f0819c94f2989b308301bb0640c2
CodeKiro <kirofiniello@outlook.it>
2024-10-22 18:37:27 +0200

    ho creato una card e utilizzato le props per passare i dati da un componente all'altro in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import PropTypes from "prop-types";
 
 function App() {
   return (
@@ -19,10 +20,37 @@ function Avatar() {
   return <img className="avatar" src="../public/c.png" alt="logo" />;
 }
 function Intro() {
-  return;
+  return (
+    <div>
+      <h1>Ciro Finiello</h1>
+      <p>Full Stack Web Developer, when i'm not coding i'd like to workout!</p>
+    </div>
+  );
 }
 function SkillList() {
-  return;
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Svelte" emoji="👶" color="orangered" />
+    </div>
+  );
 }
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+Skill.propTypes = {
+  skill: PropTypes.string.isRequired,
+  emoji: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+};
 
 export default App;
